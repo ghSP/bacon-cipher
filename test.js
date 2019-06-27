584f3b785c7f94be4647079fb90916cb3c14c6d1
yaas-dev <gdkoco1@gmail.com>
2019-06-27 10:23:32 -0500

    Test File Corrected

--- test.js
+++ test.js
@@ -5,16 +5,19 @@ const DECODED_CAPS = cipher.decode(ENCODED, false);
 const DECODED_TITLE = cipher.decode(ENCODED);
 if (ENCODED !== 'BAABBAABAABAABABAABBABAAAABBABAABBA BABBAABBBABAAABAAABBBAABA BABBAABBBABAAABABABA!') {
   console.error('Encoding not working!');
+  console.log(ENCODED);
   process.exit(1);
 }
 console.log('Encoding working!');
 if (DECODED_CAPS !== 'TESTING WORDS WORK!') {
   console.error('Decoding (CAPS) not working!');
+  console.log(DECODED_CAPS);
   process.exit(1);
 }
 console.log('Decoding (CAPS) working!');
 if (DECODED_TITLE !== 'Testing Words Work!') {
   console.error('Decoding (TITLE) not working!');
+  console.log(DECODED_TITLE);
   process.exit(1);
 }
 console.log('Decoding (TITLE) working!')
