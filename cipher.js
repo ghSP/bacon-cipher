289945a1d0f665152404f8082aa441a8799c3f8f
yaas-dev <gdkoco1@gmail.com>
2019-06-26 22:17:01 -0500

    Initial Commit

---
+++ cipher.js
@@ -0,0 +1,55 @@
+/**
+ * @copyright 2019 Sam Price
+ * @description A Bacon Cipher
+ */
+
+
+const { cipher, orig } = require('./dict');
+
+function encode(text = '') {
+  const final = [];
+  const split = text.split('');
+  for (let i = 0; i < split.length; i++) {
+    const char = split[i].toUpperCase();
+    if (orig.includes(char)) {
+      const index = orig.indexOf(char);
+      final.push(cipher[index]);
+    } else {
+      final.push(char);
+    }
+  }
+  return final.join('');
+}
+
+function decode(text = '') {
+  const final = [];
+  const split = text.split('');
+  let charGroup = [];
+  for (let i = 0; i < split.length; i++) {
+    const char = split[i].toUpperCase();
+    if (char == 'A' || char == 'B') {
+      if (charGroup.length === 4) {
+        charGroup.push(char)
+        const index = cipher.indexOf(charGroup.join(''));
+        final.push(orig[index]);
+        charGroup = [];
+      } else {
+        charGroup.push(char);
+      }
+    } else {
+      final.push(char);
+    }
+  }
+  return titleCase(final.join(''));
+}
+
+function titleCase(str = '') {
+  return str.toLowerCase().split(' ').map(word => {
+    return word.replace(word.charAt(0), word.charAt(0).toUpperCase());
+  }).join(' ');
+}
+
+module.exports = {
+  encode,
+  decode
+}
