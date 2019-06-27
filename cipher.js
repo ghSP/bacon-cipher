73a07aae1ce3823fe66ba491e88a95ced0b92dd9
yaas-dev <gdkoco1@gmail.com>
2019-06-26 22:49:07 -0500

    Added minified version

--- cipher.js
+++ cipher.js
@@ -3,8 +3,8 @@
  * @description A Bacon Cipher
  */
 
-
-const { cipher, orig } = require('./dict');
+const cipher = ['AAAAA', 'AAAAB', 'AAABA', 'AAABB', 'AABAA', 'AABAB', 'AABBA', 'AABBB', 'ABAAA', 'ABAAB', 'ABAAB', 'ABABA', 'ABABB', 'ABBAA', 'ABBAB', 'ABBBA', 'ABBBB', 'BAAAA', 'BAAAB', 'BAABA', 'BAABB', 'BABAB', 'BABAA', 'BABAB', 'BABBA', 'BABBB'];
+const orig = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 function encode(text = '') {
   const final = [];
--- dict.js
+++
@@ -1,7 +0,0 @@
-const cipher = ['AAAAA', 'AAAAB', 'AAABA', 'AAABB', 'AABAA', 'AABAB', 'AABBA', 'AABBB', 'ABAAA', 'ABAAB', 'ABAAB', 'ABABA', 'ABABB', 'ABBAA', 'ABBAB', 'ABBBA', 'ABBBB', 'BAAAA', 'BAAAB', 'BAABA', 'BAABB', 'BABAB', 'BABAA', 'BABAB', 'BABBA', 'BABBB'];
-const orig = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-module.exports = {
-  cipher,
-  orig
-}
